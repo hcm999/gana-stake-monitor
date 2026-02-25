5279f67bddd800df78e44b08b26b327a3f2efe05
hcm999 <601483868@qq.com>
2026-02-26 01:55:43 +0800

    Update config.js

--- api/config.js
+++ api/config.js
@@ -1,24 +1,27 @@
-// 配置信息
 export const CONFIG = {
     STAKING: "0x72212F35aC448FE7763aA1BFdb360193Fa098E52",
     LP_POOL: "0xa2f464a2462aed49b9b31eb8861bc6b0bbb0483f",
     USDT: "0x55d398326f99059fF775485246999027B3197955",
     
-    // BSC节点
+    // BSC节点 - 增加更多节点
     BSC_NODES: [
         'https://bsc-dataseed1.binance.org',
         'https://bsc-dataseed2.binance.org',
         'https://bsc-dataseed3.binance.org',
-        'https://bsc-dataseed4.binance.org'
+        'https://bsc-dataseed4.binance.org',
+        'https://bsc-dataseed1.defibit.io',
+        'https://bsc-dataseed2.defibit.io',
+        'https://bsc-dataseed1.ninicoin.io',
+        'https://bsc-dataseed2.ninicoin.io'
     ],
     
-    // 查询配置
-    BATCH_SIZE: 10,
-    PARALLEL_BATCHES: 2,
-    RETRY_LIMIT: 3,
-    RETRY_DELAY: 1000,
+    // 查询配置 - 减小批次大小
+    BATCH_SIZE: 5,           // 从10减小到5
+    PARALLEL_BATCHES: 1,     // 从2减小到1
+    RETRY_LIMIT: 5,          // 从3增加到5
+    RETRY_DELAY: 2000,       // 从1000增加到2000
     
-    // Vercel KV存储 (需要配置)
+    // Vercel KV存储
     KV: {
         DATA_KEY: 'staking_data',
         ADDRESS_KEY: 'address_list',
@@ -32,22 +35,3 @@ export const CONFIG = {
         2: 2592000   // 30天
     }
 };
-
-// 质押周期标签
-export const STAKE_DURATION_LABELS = {
-    0: "1天",
-    1: "15天",
-    2: "30天"
-};
-
-// USDT ABI
-export const USDT_ABI = [
-    "function balanceOf(address account) view returns (uint256)",
-    "function decimals() view returns (uint8)"
-];
-
-// 质押合约ABI (精简版，只保留需要的函数)
-export const STAKING_ABI = [
-    "function stakeCount(address user) view returns (uint256)",
-    "function userStakeRecord(address user, uint256 index) view returns (uint40 stakeTime, uint160 amount, bool status, uint8 stakeIndex)"
-];
